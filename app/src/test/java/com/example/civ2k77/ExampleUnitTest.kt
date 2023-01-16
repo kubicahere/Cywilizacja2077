584a6dfc61effd8bf4392a63f74c55545f172a42
kubicahere <kubica.here@gmail.com>
2023-01-16 20:43:59 +0100

    Added few unit tests

--- app/src/test/java/com/example/civ2k77/ExampleUnitTest.kt
+++ app/src/test/java/com/example/civ2k77/ExampleUnitTest.kt
@@ -14,4 +14,31 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+    @Test
+    fun test_checkPasswords() {
+        val form = RegisterFragmentClass()
+        form.passwordTest = "123"
+        form.confPasswordTest = "123"
+        assertEquals(form.checkPasswords(), true)
+    }
+    @Test
+    fun test_clearingStrings() {
+        val form = RegisterFragmentClass()
+        form.loginTest = "testRegister"
+        form.passwordTest = "123"
+        form.confPasswordTest = "123"
+        form.clearInputs()
+        assertEquals(form.loginTest, null)
+        assertEquals(form.passwordTest, null)
+        assertEquals(form.confPasswordTest, null)
+    }
+    @Test
+    fun check_clearingStringsLogin() {
+        val form = LoginFragmentClass()
+        form.loginLogin = "testLogin"
+        form.loginPassword = "testPassword"
+        form.clearInputs()
+        assertEquals(form.loginLogin, null)
+        assertEquals(form.loginPassword, null)
+    }
 }
